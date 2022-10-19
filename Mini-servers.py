8e16a0b15e8edc12ee0c59f9c7ea39cf7767f62b
JiazhengLiuUTD <112774768+JiazhengLiuUTD@users.noreply.github.com>
2022-10-19 17:30:13 -0500

    Update Mini-servers.py

--- Mini-servers.py
+++ Mini-servers.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import subprocess
 import json
 from flask import Flask
 from flask import request
 
 
-# In[ ]:
 
 
 def deploy_container(workflow_dict):
