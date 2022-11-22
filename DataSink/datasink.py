9f538c3b3c2c5cbbfb0b9848738206d625d616e5
aditichak <missaditichak@gmail.com>
2022-11-22 11:57:54 -0600

    modified datasink and ModelTest

--- DataSink/datasink.py
+++ DataSink/datasink.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 21 14:12:05 2022
-
-@author: Akash MSI
-"""
-
 from flask import Flask, render_template, request, json,redirect, url_for, jsonify
 app = Flask(__name__)
 
-
+testData = []
 workflowId = ""
 res = ""
 
@@ -16,24 +9,18 @@ res = ""
 def create():
     global workflowId 
     global res
+    global testData
     workflowId = request.json["WFID"]
     res = request.json["DATA"]
+    testData.append((workflowId, res))
     return redirect(url_for('index'))
 
 
 @app.route('/index/', methods=['GET'])
 def index():
-    # if request.args:
-    #     workflowId = request.args.get("wfid")
-    #     res = request.args.get("content")
-    #     return render_template("index.html",  flow=workflowId, con=res)
-    # return render_template("index.html")
-    return render_template("index.html",  flow=workflowId, cons=res)
+    return render_template("index.html",  testData=testData)
 
-@app.route('/data', methods=['GET'])
-def data():
-    return jsonify(flow=workflowId, cons=res)
 
 
 if __name__ == "__main__":
-    app.run(host='http://10.176.67.247', port=9090)
+    app.run(host='10.176.67.247', port=9090)
