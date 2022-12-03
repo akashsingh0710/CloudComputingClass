d5f9b848b8ed9df62d2c1aed6410bb402d1c4e7a
ThorRydahl <43283257+ThorRydahl@users.noreply.github.com>
2022-12-03 13:06:59 -0600

    update datagen message passing

--- Workflow Manager/workflow_router.py
+++ Workflow Manager/workflow_router.py
@@ -13,24 +13,19 @@ ip_table = {} # vm:ip_string // UPDATED BY DATAFLOW MANAGER.
 container_table = {} # containerID:port // UPDATED LOCALLY. ONLY STORES LOCAL CONTAINERS.
 routing_table = {} # (containerID, workflow):set((nextVMID, nextContainerID)) // UPDATED BY DATAFLOW MANAGER.
 
-active_containers = {} #(tuple of image_name, port)
-active_WFID_for_data_gen1 = []  
-active_WFID_for_data_gen2 = []  
+data1_recipients = [] # (containerID, WFID)
+data2_recipients = []
+DATA1_IMG = ['aditichak/initial-process']
+DATA2_IMG = ['aditichak/modeltest']
+
+#active_containers = {} #(tuple of image_name, port)
+#active_WFID_for_data_gen1 = []  
+#active_WFID_for_data_gen2 = []  
 port_opened = []
-
-net = {
-	"M1": "10.176.67.108",
-	"M2": "10.176.67.111",
-	"M3": "10.176.67.248",
- 	"M4": "10.176.67.247",
-	"M5": "10.176.67.246",
-	"M6": "10.176.67.245"
-}
-
 
 # Container Table reverse lookup
 def get_container(port):
-    print("port in get_cotainer: ", port)
+    print("port in get_container: ", port)
     # print("port type in get_cotainer: ", type(port))
     print(container_table)
     # print(container_table)
@@ -63,8 +58,8 @@ def getAddr(vm, port=ROUTING_PORT, path = None):
 # Takes a post request with a component dict (fields: image, cid, port)
 @app.route('/deploy', methods=['POST'])
 def deploy_container():
-    global active_WFID_for_data_gen1
-    global active_WFID_for_data_gen2
+    global data1_recipients, data2_recipients
+    global DATA1_IMG, DATA2_IMG
     global port_opened
     
     if request.method == 'POST':
@@ -111,33 +106,18 @@ def deploy_container():
         #     print("container ID deployed: " , result)
         # else:
         #     print("Reusing persistent container!!")
-            
-        active_container_key =  str(service['cid']) + " " + str(service["WFID"])
-        active_containers[active_container_key] = (service['image'] , service['port'], service["WFID"])
-        
-        
-        if WFID not in active_WFID_for_data_gen1:
-            active_WFID_for_data_gen1.append(WFID)
-            
-        if WFID not in active_WFID_for_data_gen2:    
-            active_WFID_for_data_gen2.append(WFID)
-            
-        active_WFID_for_data_gen1.sort()
-        active_WFID_for_data_gen2.sort()
-            
-        print("active WFID for data gen1: " , active_WFID_for_data_gen1)
-        print("active WFID for data gen2: " , active_WFID_for_data_gen2)
-        
-        
         image = service['image']
-        wfid = service["WFID"]
+        if image in DATA1_IMG:
+            data1_recipients.append((service['cid'], service['WFID']))
+        if image in DATA2_IMG:
+            data2_recipients.append((service['cid'], service['WFID']))
         
         if(image == 'aditichak/initial-process'):
-            print("start first container: image: " , str(image) , " with wfid: ", str(wfid) , " and port: ", str(port))
+            print("start first container: image: " , str(image) , " with wfid: ", str(WFID) , " and port: ", str(port))
             # router_addr = "http://{}:{}/datasink".format(net[vmID], port)
            
             WFID_JSON = {}
-            WFID_JSON['WFID'] = str(wfid)
+            WFID_JSON['WFID'] = str(WFID)
                 
             
                 # print("Inside TRy of DatgEn1")
@@ -151,7 +131,7 @@ def deploy_container():
                     # print("Afsafa: " , r)
                     print("status_code: " , r.status_code)
                     if r.status_code == 200 and r.text == "200 OK":
-                        print("DataFlowManager sucessfully started image: {}, port: {}, wfid: {}".format(image , port, wfid))
+                        print("DataFlowManager sucessfully started image: {}, port: {}, wfid: {}".format(image , port, WFID))
                         # active_containers.pop(service['cid'])
                         print("sent data to {}".format(router_addr))
                         a=1
@@ -313,121 +293,36 @@ def pass_data_generator1():
     # print("From pass datagen, the recieved json is: " , opinionsDict)
     # opinionsDict['PORT'] = 0
     
-    print("active_WFID_for_data_gen1: " , active_WFID_for_data_gen1)
-    if(len(active_WFID_for_data_gen1)>0):
-        # opinionsDict['WFID'] = active_WFID_for_data_gen1[0]
-        
-        print("active containers from data gen1: " , active_containers)
-        for key,value in active_containers.copy().items():
-            image , port, wfid  = active_containers[key]
-            # opinionsDict['PORT'] = int(port)
-            if(image == 'aditichak/initial-process'):
-                print("pass_data_generator1: image: " , image , " found! with wfid: ", wfid , "and port: ", port)
-                router_addr = "http://{}:{}/trainingdata".format(net[vmID], port)
-            
-                try:
-                    # print("Inside TRy of DatgEn1")
-                    # print("opinionsDict: /n" + opinionsDict)
-                    while True:
-                        r = requests.post(router_addr, json=opinionsDict)
-                        # print("Afsafa: " , r)
-                        print("status_code: " , r.status_code)
-                        if r.status_code == 200 and r.text == "200 OK":
-                            print("Data from datagen1 to container passed sucessfully, image: {}, port: {}, wfid: {}".format(image , port, wfid))
-                            active_containers.pop(key)
-                            print("sent data to {}".format(router_addr))
-                            break
-                except Exception as e:
-                     r=requests.post("http://127.0.0.1:{}/datasink".format(port))
-                     if r.status_code == 200 and r.text == "200 OK":
-                            print("The container was in local machine!!")
-                            print("Data from datagen1 to container passed sucessfully, image: {}, port: {}, wfid: {}".format(image , port, wfid))
-                            active_containers.pop(key)
-                            print("sent data to {}".format(router_addr))
-                            break
-                     print("Exception from pass_data_generator1")
-                     print(e)
-            
+    threads = []
+    for cid, wfid in data1_recipients:
+        local_vm_addr = "http://127.0.0.1:{}/datasink".format(container_table[cid])
+        x = threading.Thread(target=send_message_repeat, 
+        args=(local_vm_addr,{'WFID':wfid, 'DATA':opinionsDict}))
+        threads.append(x)
+        x.start()
+    for t in threads:
+        t.join()
+
     return '200 OK' 
-    
-    
     
 @app.route('/pass_data_gen2', methods=['POST'])
 def pass_data_generator2():
+    testDict = request.json
     
-    print("Yulaaaluuuuauauauau!!")
-    TestDict = request.json
-    
-    # print("From pass data_gen2, the recieved json is: " , TestDict)
-    # TestDict['PORT'] = 0
-    print("active_WFID_for_data_gen2: " , active_WFID_for_data_gen2)
-    
-    if(len(active_WFID_for_data_gen2)>0):
-        print("I'm in data_gen2_pass Yayy!!")
-        # TestDict['WFID'] = active_WFID_for_data_gen2[0]
-        
-        print("active containers from data gen2 please please: " , active_containers)
-        # print("Yahooo1")
-        for key,value in active_containers.copy().items():
-            # print("Yahooo2")
-            # print("this is the key from datagen2: ", key , "in activecontainer")
-            image , port, wfid  = active_containers[key]
-            # TestDict['PORT'] =  int(port)
-            print("image from data_gen2: " , image)
-            if(image == 'aditichak/prediction'):    
-                print("pass_data_generator2: image: " , image , " found! with wfid: ", wfid , "and port: ", port)
-                router_addr = "http://{}:{}/testdata".format(net[vmID], port)
-                print("sent to: ", router_addr)
-                try:
-                    r = requests.post(router_addr, json=TestDict)
-                    print("status_code: " , r.status_code)
-                    if r.status_code == 200 and r.text == "200 OK":
-                        print("Data from datagen2 to container passed sucessfully, image: {}, port: {}, wfid: {}".format(image , port, wfid))
-                        active_containers.pop(key)
-                        print("sent data to {}".format(router_addr))
-                except Exception as e:
-                    r = requests.post("http://127.0.0.1:{}/testdata".format(port))
-                    if r.status_code == 200 and r.text == "200 OK":
-                            print("The container was in local machine!!")
-                            print("Data from datagen2 to container passed sucessfully, image: {}, port: {}, wfid: {}".format(image , port, wfid))
-                            active_containers.pop(key)
-                            print("sent data to {}".format(router_addr))
-                            break
-                    print("Exception from pass_data_generator2")
-                    print(e)
+    threads = []
+    for cid, wfid in data2_recipients:
+        local_vm_addr = "http://127.0.0.1:{}/datasink".format(container_table[cid])
+        x = threading.Thread(target=send_message_repeat, 
+        args=(local_vm_addr,{'WFID':wfid, 'DATA':testDict}))
+        threads.append(x)
+        x.start()
+    for t in threads:
+        t.join()            
             
-        # print("out of loop active containers from data gen2:")            
-            
-    return '200 OK'     
-
-
+    return '200 OK'
 
 @app.route('/terminate_workflow', methods=['POST'])
-def terminate_workflow():
-    
-   
-    wfid = int(request.json['WFID'])
-    print("terminate workflow called for: ", wfid)
-    print("terminate active_WFID_for_data_gen1 called for: ", active_WFID_for_data_gen1)
-    print("terminate active_WFID_for_data_gen2 called for: ", active_WFID_for_data_gen2)
-    
-    print("wfid type: " , type(wfid))
-    if active_WFID_for_data_gen1:
-         print("active_WFID_for_data_gen1 type: " , type(active_WFID_for_data_gen1))
-    
-    c = active_WFID_for_data_gen1.count(wfid)
-    print("c: ", c)
-    for i in range(c):
-        print("removing from datagen1_list: " , wfid)
-        active_WFID_for_data_gen1.remove(wfid)
-        
-     
-    d = active_WFID_for_data_gen2.count(wfid)
-    print("d: ", d)  
-    for j in range(d) :
-        print("removing from datagen2_list: " , wfid)
-        active_WFID_for_data_gen2.remove(wfid)
-              
+def terminate_workflow():              
     # print("wifd: {} removed".format(wfid))        
             
     return '200 OK'  
